8cf456ebebad9cd7b235babbb6865c4be930aa67
MadOS <bondarna2004@mail.ru>
2023-04-25 16:53:43 +1000

    Revert "v0.3.1650 alpha"
    This reverts commit 6fc209cfb2ada088cd79537a7b60c493ca5b1cba.

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace InfoBase
             const bool isDebug = false;
 
             //для верхней панели
-            const string version = "v0.3.1650 alpha";
+            const string version = "v0.2.2051 alpha";
             const string info = "Auditions" + " " + version;
 
             //прописываем настройки консоли
@@ -30,48 +30,30 @@ namespace InfoBase
             //константы для файлов 
             string workDir = Directory.GetCurrentDirectory() + @"\data\";
             string daysDir = workDir + @"days\";
-            string logsDir = workDir + @"logs\";
 
             //настройка для EPPlus 
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
             //подготовка датабазы 
-            DataBase db = new DataBase(logsDir);
+            DataBase db = new DataBase();
             if (!File.Exists(workDir + "Data.xlsx")) db.CreateDataList(workDir + "Data.xlsx");
             if (!File.Exists(workDir + "Users.xlsx")) db.CreateUserList(workDir + "Users.xlsx");
-            if (Directory.GetFiles(daysDir, "*.txt").Length == 0) db.CreateDayList("15.01.2001");
+
+            db.CreateDayList("18.09.2021");
 
             //заполнение данных и проверка на подлинность (непустоту) 
-            if (!db.FillUsers(workDir + "Users.xlsx"))
+            if (db.FillData(workDir + "Data.xlsx") && db.FillUsers(workDir + "Users.xlsx") && db.FillDays(daysDir))
             {
-                db.LogState("Проблема со списком пользователей или ошибка FillUsers()");
-                Console.WriteLine($"Возникла ошибка, проверьте лог {DateTime.Now.ToString("dd-MM-yyyy")}.log");
-                Console.ReadKey();
-            }
-            else if (!db.FillData(workDir + "Data.xlsx"))
-            {
-                db.LogState("Проблема с базовыми данными или ошибка FillData()");
-                Console.WriteLine($"Возникла ошибка, проверьте лог {DateTime.Now.ToString("dd-MM-yyyy")}.log");
-                Console.ReadKey();
-            }
-            else if (!db.FillDays(daysDir))
-            {
-                db.LogState("Проблема с данными расписания или ошибка FillDays()");
-                Console.WriteLine($"Возникла ошибка, проверьте лог {DateTime.Now.ToString("dd-MM-yyyy")}.log");
-                Console.ReadKey();
-            }
+                Console.WriteLine("Жмакай любую клавишу"); Console.ReadKey();
 
-            //работа программы
-            else
-            {
                 while (true)
                 {
-                    User user1 = db.GetUser("login1", "Логин");
-                    User user2 = db.GetUser("ИмяАдмина1", "Имя");
-                    User user3 = db.GetUser("ИмяАдмина2", "НеИмя");
-                    Console.WriteLine("Жмакай любую клавишу"); Console.ReadKey();
                     /*место  для пары строчек кода*/
                 }
+            }
+            else
+            {
+                Console.WriteLine("Жмакай любую клавишу, у тебя ошибка :>"); Console.ReadKey();
             }
         }
     }
